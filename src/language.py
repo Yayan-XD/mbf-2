01332d70ec3ededbb8d8f324df97a70a78cf8a74
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-01-07 15:11:57 +0700

    Update language.py

--- src/language.py
+++ src/language.py
@@ -1,1 +1,14 @@
-#
+#!usr/bin/python2.7
+# coding=utf-8
+
+from bs4 import BeautifulSoup as parser
+
+def main(cookie, url, config):
+	try:
+		response = config.httpRequest(url+'/language.php', cookie).encode('utf-8')
+		html = parser(response, 'html.parser')
+		for x in html.find_all('a'):
+			if 'Bahasa Indonesia' in str(x):
+				config.httpRequest(url+x['href'], cookie)
+				break
+	except: pass
