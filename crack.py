8d6b7ce0cd395d30b7427870314adc5d30a8f817
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-01-27 15:10:08 +0700

    Create crack.py

---
+++ crack.py
@@ -0,0 +1,28 @@
+#!usr/bin/python2.7
+# coding=utf-8
+
+import os, sys, shutil
+from app import main as app
+
+base_url = 'https://mbasic.facebook.com'
+
+if sys.version_info.major != 2:
+	sys.exit('\n\033[0;97m{\033[0;92mWARNING\033[0;97m} \033[0;91mTools ini menggunakan bahasa python2.\nsegera upgrade terlebih dahulu\033[0m')
+
+try: shutil.rmtree('app/__pycache__')
+except: pass
+try: shutil.rmtree('src/__pycache__')
+except: pass
+
+for filename in os.listdir('app'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('app/'+filename)
+		except: pass
+
+for filename in os.listdir('src'):
+	if filename[-3:] == 'pyc':
+		try: os.remove('src/'+filename)
+		except: pass
+#Lo Kontol#
+awokawokawok = app.Brute(base_url)
+awokawokawok.start()
